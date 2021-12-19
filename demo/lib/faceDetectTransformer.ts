888113693aa8e7f6d95255fd752894ec5d61fcbe
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-12-19 03:51:16 +0000

    prettier

--- demo/lib/faceDetectTransformer.ts
+++ demo/lib/faceDetectTransformer.ts
@@ -1,12 +1,14 @@
-import type {WasmModule} from './faceDetectWasmModule'
+import { WasmModule } from './faceDetectWasmModule'
 
 declare var VideoFrame: {
-  prototype: VideoFrame;
+  prototype: VideoFrame
   // new(source: CanvasImageSource | OffscreenCanvas, init?: VideoFrameInit): VideoFrame;
-  new(data: AllowSharedBufferSource, init: VideoFrameBufferInit): VideoFrame;
-};
+  new (data: AllowSharedBufferSource, init: VideoFrameBufferInit): VideoFrame
+}
 
-export function getFaceDetectTransformer (faceDetectModule: WasmModule): (
+export function getFaceDetectTransformer (
+  faceDetectModule: WasmModule
+): (
   videoFrame: VideoFrame,
   controller: TransformStreamDefaultController<VideoFrame>
 ) => void {
@@ -16,41 +18,60 @@ export function getFaceDetectTransformer (faceDetectModule: WasmModule): (
   const outputImageOffset = faceDetectModule.getOutputImageBuffer()
 
   return (videoFrame, controller) => {
-    try{
-    const opts = {rect: {x:0, y:0, width: videoFrame.codedWidth, height: videoFrame.codedHeight}}
-    const size = videoFrame.allocationSize(opts)
-    // console.log(
-    //   'videoFrame.format',
-    //   videoFrame.format,
-    //   videoFrame.codedWidth,
-    //   videoFrame.codedHeight,
-    //   videoFrame.allocationSize(opts)
-    // )
-    videoFrame.copyTo(faceDetectModule.HEAPU8.subarray(inputImageOffset, inputImageOffset + size), opts)
-    try{
-      faceDetectModule.detectFace(videoFrame.codedWidth, videoFrame.codedHeight)
-    }catch (ex) {
-      if(typeof(ex) === 'number') {
-        console.log(faceDetectModule.getExceptionMsg(ex as number))
-      }else {
-        console.log(ex)
+    try {
+      const opts = {
+        rect: {
+          x: 0,
+          y: 0,
+          width: videoFrame.codedWidth,
+          height: videoFrame.codedHeight
+        }
       }
-      controller.enqueue(videoFrame)
-      return
+      const size = videoFrame.allocationSize(opts)
+      // console.log(
+      //   'videoFrame.format',
+      //   videoFrame.format,
+      //   videoFrame.codedWidth,
+      //   videoFrame.codedHeight,
+      //   videoFrame.allocationSize(opts)
+      // )
+      videoFrame.copyTo(
+        faceDetectModule.HEAPU8.subarray(
+          inputImageOffset,
+          inputImageOffset + size
+        ),
+        opts
+      )
+      try {
+        faceDetectModule.detectFace(
+          videoFrame.codedWidth,
+          videoFrame.codedHeight
+        )
+      } catch (ex) {
+        if (typeof ex === 'number') {
+          console.log(faceDetectModule.getExceptionMsg(ex as number))
+        } else {
+          console.log(ex)
+        }
+        controller.enqueue(videoFrame)
+        return
+      }
+      const output = faceDetectModule.HEAPU8.subarray(
+        outputImageOffset,
+        outputImageOffset + size
+      )
+      const newFrame = new VideoFrame(output, {
+        codedHeight: videoFrame.codedHeight,
+        codedWidth: videoFrame.codedWidth,
+        format: videoFrame.format!,
+        duration: videoFrame.duration!,
+        timestamp: videoFrame.timestamp!,
+        visibleRect: videoFrame.visibleRect!
+      })
+      videoFrame.close()
+      controller.enqueue(newFrame)
+    } catch (ex) {
+      console.log(ex)
     }
-    const output = faceDetectModule.HEAPU8.subarray(outputImageOffset, outputImageOffset + size)
-    const newFrame = new VideoFrame(output, {
-      codedHeight: videoFrame.codedHeight,
-      codedWidth: videoFrame.codedWidth,
-      format: videoFrame.format!,
-      duration: videoFrame.duration!,
-      timestamp: videoFrame.timestamp!,
-      visibleRect: videoFrame.visibleRect!,
-    })
-    videoFrame.close()
-    controller.enqueue(newFrame)
-  }catch(ex) {
-    console.log(ex)
-  }
   }
 }
